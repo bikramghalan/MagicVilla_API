1acb2e83659c4cb20c18ce74f0b8c2a7e30260c4
bikram ghalan <bikramghalan1998@gmail.com>
2023-03-28 00:11:27 +1100

    HttpPost and HttpGet created

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,69 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]  //Get End point
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+
+        }
+        [HttpGet("{id:int}",Name ="GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+
+        public ActionResult<VillaDTO> GetVillas(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            
+            }
+            
+            var villa=VillaStore.villaList.FirstOrDefault(u=> u.Id == id);
+            if (villa == null) 
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO) 
+        {
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO); 
+
+            return CreatedAtRoute("GetVilla", new {id=villaDTO.Id},villaDTO);
+
+        }
+            
+
+    }
+    
+}
+
